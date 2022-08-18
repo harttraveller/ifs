89a139e2f0f242085c7179e11f365d3b618dbd0b
Hart Traveller <hartktraveller@gmail.com>
2022-08-18 11:18:37 -0400

    add sierpinski4d

--- ifs/visualize.py
+++ ifs/visualize.py
@@ -23,7 +23,7 @@ def v4D(points, save):
                 z=c,
                 mode="markers",
                 marker=dict(
-                    size=2,
+                    size=1.5,
                     color=a,  # set color equal to a variable
                     colorscale="Viridis",  # one of plotly colorscales
                     showscale=False,
@@ -37,15 +37,19 @@ def v4D(points, save):
         autosize=False,
         width=500,
         height=500,
-        margin=dict(l=10, r=10, b=10, t=10, pad=0),
+        margin=dict(l=10, r=10, b=10, t=40, pad=10),
     )
+    camera = dict(
+        up=dict(x=0, y=0, z=1), center=dict(x=0, y=0, z=0), eye=dict(x=0, y=2.5, z=0)
+    )
+    fig.update_layout(scene_camera=camera, title="interactive graph")
     if save is not None:
         fig.write_html(save)
     else:
         fig.show()
 
 
-def visualize(points, save):
+def visualize(points, save=None):
     dimensions = len(points[0])
     assert dimensions in {1, 2, 3, 4}
     if dimensions == 1:
