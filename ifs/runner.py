4871006493958d94d33967dfbcca660dab99d38e
Hart Traveller <hartktraveller@gmail.com>
2022-08-27 19:50:36 -0400

    ifs/runner: adding in skeleton code

--- ifs/runner.py
+++ ifs/runner.py
@@ -8,24 +8,41 @@ This is the class to generate an IFS fractal. It requires:
 
 """
 
+import numpy as np
+from types import FunctionType
+from typing import Dict
+
 
 class IFS:
     def __init__(self) -> None:
         pass
 
+    # Private Methods
+    def __resolve_dimensionality(self, vertices: np.array) -> int:
+        """
+        Resolves the number of dimensions the vertices are expressed in.
+        ? Surely a better way to articulate.
+        """
+        pass
+
+    # Public Methods
+
     def reset(self) -> None:
         pass
 
-    def add_vertices(self, vertices):
+    def add_vertices(self, vertices: np.array):
+        """
+        Adds the edge vertices.
+        """
         pass
 
-    def add_midpoint_function(self, midpoint, params):
+    def add_midpoint_function(self, midpoint: FunctionType, params: Dict) -> None:
         """
         A midpoint function and the parameters for the functions execution.
         """
         pass
 
-    def add_ruleset_function(self, ruleset, params):
+    def add_ruleset_function(self, ruleset: FunctionType, params: Dict) -> None:
         """
         A ruleset function and the parameters for the functions execution.
         """
